c0b90f6f674779f93b8087445027bb7cea07a1fb
EfeDenizerKoprulu <efedenizerkoprulu@outlook.com>
2023-01-07 21:20:21 +0300

    V1

--- Components/PartiteComponent/Pointer.js
+++ Components/PartiteComponent/Pointer.js
@@ -7,9 +7,9 @@ const Pointer = (props) => {
     <div
       className={`h-fit w-fit z-20 rounded-full bg-[#0948B3] p-[6px] ${aa[props.Class]} items-center absolute`}
     >
-      <div className="md:block absolute md:-1 text-sm bg-black md:-top-[28px] -top-[25px] -left-12 w-24 text-center z-30 text-white rounded-l">
+      <div className="md:block absolute md:p-1 text-sm bg-black md:-top-[32px] -top-[25px] -left-12 w-24 text-center z-30 text-white rounded-l">
         {props.Description}
-        <span className="absolute -rotate-90 text-[26px] md:top-2 top-2 text-black right-8">
+        <span className="absolute -rotate-90 text-[26px] md:top-4 top-2 text-black right-8">
           <IoMdArrowDropleft />
         </span>
       </div>
